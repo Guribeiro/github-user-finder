209b37ed870cb6e9411c18993eed53d240818792
Gustavo Henrique <gugahribeiro@hotmail.com>
2020-05-30 18:15:00 -0300

    add state property from component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,25 +1,62 @@
 import React, { Component } from 'react';
 
+import { Link } from 'react-router-dom';
+
 import { AiFillGithub } from 'react-icons/ai'
 import { MdNotificationsActive, MdAdd } from 'react-icons/md'
 
-import imgProfile from '../../assets/avatar_empty.jpg';
+//import imgProfile from '../../assets/avatar_empty.jpg';
 
 import './style.scss';
 
 class Header extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            user: []
+        }
+        this.handleProfile = this.handleProfile.bind(this);
+
+    }
+
+
+    handleProfile(event) {
+        event.preventDefault();
+        const api = `https://api.github.com/users/${this.state.github_username}`
+
+        localStorage.setItem('user/profile', this.state.profile);
+
+        fetch(api)
+            .then((response) => response.json())
+            .then((json) => {
+
+                if (json) {
+                    this.setState({ user: json })
+                    this.setState({ redirectToReferrer: true })
+                    document.location = `/user/${this.state.github_username}`;
+                }
+            })
+    }
+
     render() {
+
         return (
             <div className="header">
                 <nav>
                     <div className="nav-bar">
                         <div className="icon-nav">
-                            <a href="/">
+                            <Link to='/'>
                                 <AiFillGithub size='38' />
-                            </a>
+                            </Link>
                         </div>
-                        <form>
-                            <input type="text" placeholder='Search here' />
+                        <form onSubmit={this.handleProfile}>
+                            <input
+                                type="text"
+                                placeholder='Search here'
+                                value={this.state.github_username}
+                                onChange={(e) => this.setState({ github_username: e.target.value })}
+                            />
                         </form>
                         <ul>
                             <li>Pull requests</li>
@@ -29,15 +66,15 @@ class Header extends Component {
                         </ul>
                     </div>
                     <div className='nav-options'>
-                        <a href="/">
+                        <Link to='/'>
                             <MdNotificationsActive size='24' />
-                        </a>
-                        <a href="/">
+                        </Link>
+                        <Link to='/'>
                             <MdAdd size='24' />
-                        </a>
-                        <a href="/">
+                        </Link>
+                        <a href={this.props.profileHtml}>
                             <figure>
-                                <img src={imgProfile} alt="" />
+                                <img src={this.props.profileImg} alt="" />
                             </figure>
                         </a>
                     </div>
