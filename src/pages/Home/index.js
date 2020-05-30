f159bacad9ce17b127d3514bb66773a3b8c85030
Gustavo Henrique <gugahribeiro@hotmail.com>
2020-05-30 18:15:54 -0300

    add footer and head

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+
 import octocat from '../../assets/Octocat.png';
 import './styles.scss';
 
 class Home extends Component {
+    
+    constructor(props){
+        super(props)
+        this.state = {
+            redirectToReferrer: this.props.redirectToReferrer
+        }
+    }
 
+    componentDidMount(){
+        console.log(this.state.redirectToReferrer)
+    }
 
     render() {
         return (
             <div className='Home'>
+                <Header redirectToReferrer={this.props.redirectToReferrer}/>
                 <div className="container">
                     <figure>
                         <img src={octocat} alt="" />
@@ -16,6 +30,7 @@ class Home extends Component {
                     <h1>Welcome to the <span>github user-finder</span></h1>
                     <p>Here you can search for any github profile you want. Check some of its informations and enjoy it.</p>
                 </div>
+                <Footer/>
             </div>
         );
     }
